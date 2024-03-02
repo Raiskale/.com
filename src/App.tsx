d97624f748ed343f77d973acf7914327fb78f810
Raiskale <arrarekt@gmail.com>
2024-03-02 22:22:43 +0200

    things

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+
+    
     const [showSplashScreen, setShowSplashScreen] = useState(true);
     const [countdown, setCountdown] = useState(3);
 
@@ -36,12 +38,18 @@ const App: React.FC = () => {
                     <h1 className="MainsiteTitle">
                         <span className="white-text">Raiskale</span>
                         <span className="green-text">.com</span>
+                        
                     </h1>
                     {/* Add your main site content here */}
                     <div className="relative h-full w-full bg-black">
                         <div className="absolute bottom-0 left-0 right-0 top-0 bg-gradient"></div>
                         <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-radial-gradient"></div>
+                        <div className="line"></div>
                     </div>
+                    <div className="container">
+      <img src=".\Green.png" alt="Small Image" className="small-image" />
+      {/* Other content of your component */}
+    </div>
                 </div>
             )}
         </div>
